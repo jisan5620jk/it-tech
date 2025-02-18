e71b489a54eec09787195a414dc058760ec51f7b
jisan5620jk <jisank5620@gmail.com>
2025-02-18 17:33:58 +0600

    respnsive

--- src/Component1/LatestWork/LatestWorkCard.jsx
+++ src/Component1/LatestWork/LatestWorkCard.jsx
@@ -9,23 +9,23 @@ const LatestWorkCard = ({
   latestWorkBtnText,
 }) => {
   return (
-    <div className='pl-[30px] pt-[30px]'>
+    <div className='sm:pl-[30px] sm:pt-[30px]'>
       <div className='bg-BodyBg3-0 rounded-lg relative z-10 before:absolute before:top-0 before:right-0 before:bg-gradient-to-br before:to-PrimaryColor2-0 before:from-PrimaryColor3-0 before:w-0 before:h-full before:transition-all before:duration-300 before:-z-10 before:rounded-lg hover:before:w-full hover:before:left-0'>
-        <div className='-ml-3 sm:-ml-[30px] relative -top-3 sm:-top-[30px]'>
+        <div className='sm:-ml-[30px] relative sm:-top-[30px]'>
           <img
             src={latestWorkCardImg}
             draggable={false}
             alt='Image'
           />
         </div>
-        <div className='px-[38px] pb-7 pt-2'>
+        <div className='px-4 sm:px-[38px] pb-7 pt-5 sm:pt-2'>
           <h6 className='text-white bg-white bg-opacity-5 font-Rajdhani font-medium uppercase text-sm rounded-full px-[26px] py-[7px] inline-block'>
             {latestWorkSubTitle}
           </h6>
           <div className='inline-block mt-4'>
             <Link
               to={latestWorkCardURL}
-              className='text-white text-2xl font-Rajdhani font-medium'
+              className='text-white text-xl sm:text-2xl font-Rajdhani font-medium'
             >
               {latestWorkTitle}
             </Link>
