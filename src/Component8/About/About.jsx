7d95d818e152c1d11c4eb5e86a07036ebe869b60
jisan5620jk <jisank5620@gmail.com>
2025-03-10 12:04:28 +0600

    Add About Component

--- src/Component8/About/About.jsx
+++ src/Component8/About/About.jsx
@@ -1,29 +1,118 @@
-import aboutThumb from '/images/AI-11.png';
+import aboutThumb from '/images/about-slider-image.jpg';
+import aboutThumb2 from '/images/about-slider-image2.jpg';
+import aboutThumb3 from '/images/about-slider-image3.jpg';
+import aboutIcon from '/images/about-icon.webp';
 import { Link } from 'react-router-dom';
 import titleShape from '/images/sub-title-shape.png';
-import shape from '/images/about-shape.png';
-import { GoCheckCircleFill } from 'react-icons/go';
+import shape from '/images/about-shape.webp';
+import { GoArrowRight, GoCheckCircleFill } from 'react-icons/go';
+import OnScrollCounter from '../../Shared/Counter/OnScrollCounter';
+import { Swiper, SwiperSlide } from 'swiper/react';
+import 'swiper/css';
+import { Autoplay } from 'swiper/modules';
+import AboutNavigation from './AboutNavigation';
 
 const About = () => {
+  const settings = {
+    loop: true,
+    spaceBetween: 30,
+    modules: [Autoplay],
+    speed: 1500,
+    autoplay: {
+      delay: 5000, // Set delay time in milliseconds
+      disableOnInteraction: false, // Keep autoplay on user interaction
+    },
+  };
   return (
-    <section className='py-16 md:py-20 lg:py-[120px] relative'>
+    <section className='py-16 md:py-20 lg:py-28 bg-BodyBg7-0 relative'>
       <div className='Container'>
-        <div className='grid grid-cols-1 lg:grid-cols-2 gap-10 md:gap-[92px] lg:gap-10 2xl:gap-[92px] items-center'>
-          <div className='relative z-10'>
-            <img
-              src={aboutThumb}
-              draggable='false'
-              className='md:w-full lg:w 2xl:-ml-[38px] 2xl:max-w-[inherit]'
-            />
-            <div className='absolute bottom-32 right-0 px-5 py-6 sm:flex gap-2 items-center animate-wiggle hidden bg-white bg-opacity-20 backdrop-filter backdrop-blur-md border-2 rounded-lg border-white'>
-              <div className='text-PrimaryColor-0'>
-                <GoCheckCircleFill size={24} />
+        <div className='flex lg:items-center justify-start lg:justify-between gap-8 lg:gap-0 border-b border-SecondaryColor-0 border-opacity-10 mb-10 pb-4'>
+          <div>
+            <h5 className='flex items-center gap-2 font-Rajdhani text-lg sm:text-xl font-semibold text-PrimaryColor-0 uppercase'>
+              <img
+                src={titleShape}
+                draggable={false}
+                alt='Shape'
+              />{' '}
+              About Us
+            </h5>
+            <h1 className='font-Rajdhani font-bold text-xl leading-7 sm:text-[34px] sm:leading-[44px] md:text-[44px] md:leading-[54px] lg:text-[30px] lg:leading-[40px] xl:text-[36px] xl:leading-[46px] 2xl:text-[80px] 2xl:leading-[82px] text-HeadingColor-0 mt-[14px] mb-4'>
+              Company Area
+            </h1>
+          </div>
+          <div className='grid grid-cols-2 items-center gap-8'>
+            <div className='flex flex-col items-center'>
+              <div className='counter-text font-Rajdhani font-bold text-transparent text-[90px] leading-[90px] flex items-center'>
+                <OnScrollCounter
+                  start={0}
+                  end={97}
+                  duration={1000}
+                />
+                <h1>%</h1>
               </div>
               <div>
-                <h5 className='font-Rajdhani font-semibold text-HeadingColor-0 text-xl'>
-                  High Performances
-                </h5>
+                <p className='font-Nunito text-TextColor2-0 -mt-2'>
+                  Assesment & Analysis
+                </p>
               </div>
+            </div>
+            <div className='flex flex-col items-center'>
+              <div className='counter-text font-Rajdhani font-bold text-transparent text-[90px] leading-[90px] flex items-center'>
+                <OnScrollCounter
+                  start={0}
+                  end={130}
+                  duration={1000}
+                />
+                <h1>+</h1>
+              </div>
+              <div>
+                <p className='font-Nunito text-TextColor2-0 -mt-2'>
+                  High Expert Technicians
+                </p>
+              </div>
+            </div>
+          </div>
+        </div>
+        <div className='grid grid-cols-1 lg:grid-cols-2 gap-10 md:gap-[92px] lg:gap-10 2xl:gap-12 items-center'>
+          <div className='relative z-10 3xl:pr-12'>
+            <Swiper {...settings}>
+              <SwiperSlide>
+                <img
+                  src={aboutThumb}
+                  draggable='false'
+                  className='w-full'
+                />
+              </SwiperSlide>
+              <SwiperSlide>
+                <img
+                  src={aboutThumb2}
+                  draggable='false'
+                  className='w-full'
+                />
+              </SwiperSlide>
+              <SwiperSlide>
+                <img
+                  src={aboutThumb3}
+                  draggable='false'
+                  className='w-full'
+                />
+              </SwiperSlide>
+              <AboutNavigation />
+            </Swiper>
+            <div className='absolute z-10 bottom-[82px] -left-[64px] pl-9 pr-[43px] py-8 hidden md:block bg-PrimaryColor2-0 rounded-t-lg rounded-l-lg'>
+              <div>
+                <img
+                  src={aboutIcon}
+                  draggable={false}
+                  alt='Icon'
+                />
+              </div>
+              <h4 className='font-Rajdhani font-semibold text-2xl text-white mt-10'>
+                Secure Your House
+                <br />Office Building And
+                <br />Protected Area
+              </h4>
+              <Link to={'/contact'} className='flex items-center justify-start gap-2 font-Rajdhani font-semibold text-lg text-white uppercase'>Contact Us<GoArrowRight size={22} /></Link>
             </div>
           </div>
           <div className='relative'>
@@ -36,11 +125,7 @@ const About = () => {
               About Us
             </h5>
             <h1 className='font-Rajdhani font-bold text-xl leading-7 sm:text-[34px] sm:leading-[44px] md:text-[44px] md:leading-[54px] lg:text-[30px] lg:leading-[40px] xl:text-[36px] xl:leading-[46px] 2xl:text-[42px] 2xl:leading-[52px] text-HeadingColor-0 mt-[14px] mb-4'>
-              Building Tomorrow’s Technology
-              <br />
-              A Commitment to Excellence in
-              <br />
-              Robotics Industry
+              Discover Our Security Story
             </h1>
             <p className='font-Nunito text-TextColor2-0 pb-7'>
               Competently leverage existing top-line bandwidth vis-a-vis
@@ -117,7 +202,7 @@ const About = () => {
         src={shape}
         draggable={false}
         alt='Shape'
-        className='absolute top-20 right-32 animate-dance2 hidden 2xl:block'
+        className='absolute bottom-10 right-0 animate-wiggle hidden 2xl:block'
       />
     </section>
   );
--- src/Pages/Home8/Home8.jsx
+++ src/Pages/Home8/Home8.jsx
@@ -1,9 +1,11 @@
+import About from "../../Component8/About/About";
 import Banner from "../../Component8/Banner/Banner";
 
 const Home8 = () => {
   return (
     <>
       <Banner />
+      <About />
     </>
   );
 };
